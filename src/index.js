c43ac59742b17c707eb1ad393afa27b76090cc27
Michał Jarek <miciu77@gmail.com>
2022-09-26 20:09:19 +0200

    Notiflix added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-
 import { gallerySet } from './js/gallerySet';
 import { fetchPicture } from './js/fetchPicture';
+import Notiflix from 'notiflix';
 
 const gallery = document.querySelector('.gallery');
 const searchForm = document.querySelector('.search-form');
@@ -10,9 +10,12 @@ let page;
 searchForm.lastElementChild.addEventListener('click', async e => {
   gallery.innerHTML = '';
   e.preventDefault();
-  page = 1; 
+  page = 1;
   let objectGallery = await fetchPicture(searchedTxt.value, page);
+  console.log(objectGallery);
+  if (objectGallery.length === 0)
+    return Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
   gallery.insertAdjacentHTML('beforeend', gallerySet(objectGallery));
 });
-
-
