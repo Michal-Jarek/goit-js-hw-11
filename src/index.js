a37de246af29a40745eba59d746842e048e2b800
Michał Jarek <miciu77@gmail.com>
2022-09-27 11:56:01 +0200

    Basic version added

--- src/index.js
+++ src/index.js
@@ -1,21 +1,50 @@
 import { gallerySet } from './js/gallerySet';
 import { fetchPicture } from './js/fetchPicture';
 import Notiflix from 'notiflix';
+import { toInteger } from 'lodash';
+
+function toggleBtn() {
+  loadMoreBtn.classList.toggle('is-hidden');
+}
 
 const gallery = document.querySelector('.gallery');
 const searchForm = document.querySelector('.search-form');
 const searchedTxt = document.querySelector('[name="searchQuery"]');
+const loadMoreBtn = document.querySelector('.load-more');
 let page;
+let objectGallery;
+const perPage = 40;
+let maxPages;
+console.log(maxPages);
 
 searchForm.lastElementChild.addEventListener('click', async e => {
+  if (!loadMoreBtn.classList.contains('is-hidden')) toggleBtn();
   gallery.innerHTML = '';
   e.preventDefault();
   page = 1;
-  let objectGallery = await fetchPicture(searchedTxt.value, page);
+  objectGallery = await fetchPicture(searchedTxt.value, page, perPage);
   console.log(objectGallery);
-  if (objectGallery.length === 0)
+  if (objectGallery.hits.length === 0)
     return Notiflix.Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
-  gallery.insertAdjacentHTML('beforeend', gallerySet(objectGallery));
+  Notiflix.Notify.success(
+    `"Hooray! We found ${objectGallery.totalHits} images."`
+  );
+  maxPages = toInteger(objectGallery.totalHits / perPage) + 1;
+  gallery.insertAdjacentHTML('beforeend', gallerySet(objectGallery.hits));
+  toggleBtn();
+});
+
+loadMoreBtn.addEventListener('click', async () => {
+  if (maxPages === page) {
+    toggleBtn();
+    return Notiflix.Notify.failure(
+      `We're sorry, but you've reached the end of search results.`
+    );
+  }
+  page++;
+  console.log(page);
+  objectGallery = await fetchPicture(searchedTxt.value, page);
+  gallery.insertAdjacentHTML('beforeend', gallerySet(objectGallery.hits));
 });
--- src/js/fetchPicture.js
+++ src/js/fetchPicture.js
@@ -1,23 +1,28 @@
 import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-const fetchPicture = (keyWord, page) => {
+const fetchPicture = (keyWord, page, perPage) => {
   return axios
     .get('?', {
       params: {
         key: '30058964-66debb9f20d9f056f9054d1c1',
         image_type: 'photo',
         orientation: 'horizontal',
-        per_page: 40,
+        per_page: perPage,
         safesearch: 'true',
         page: page,
         q: keyWord,
       },
     })
-    .then(response => response.data.hits)
+    .then(response => {
+       console.log(response.data);
+      return response.data;
+     
+    })
     .catch(error => {
       console.log(error);
-      //   return new error();
     });
 };
+
+
 export { fetchPicture };
